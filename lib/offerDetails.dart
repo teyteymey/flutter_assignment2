e5030e5d505ce0e0bcae9001c3095cfe73285ef1
teyteymey <tatiana.meyer.u@gmail.com>
2022-04-02 14:30:11 +0200

    details page: image, title, location, date and divider

--- lib/offerDetails.dart
+++ lib/offerDetails.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'globalVar.dart' as globals;
 import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 
 class offerDetails extends StatefulWidget {
   List<String> offerDetailsList = [];
@@ -13,7 +14,9 @@ class offerDetails extends StatefulWidget {
   State<offerDetails> createState() => _offerDetails(offerDetailsList);
 }
 
+// this class builds the details of a certain offer
 class _offerDetails extends State<offerDetails> {
+  // ATTRIBUTES
   List<String> offerDetailsList = [];
   String title = "default";
   // ignore: non_constant_identifier_names
@@ -21,18 +24,22 @@ class _offerDetails extends State<offerDetails> {
   String distance = "default";
   //default image
   String image = "default";
-  String city = "Deventer (default)"; //todo
+  String city = "default"; //todo
   String description = "default";
 
+  // BUILDER METHOD
+  // we set the attributes of the offer
   _offerDetails(List<String> offerDetailsList) {
     this.offerDetailsList = offerDetailsList;
     title = offerDetailsList.elementAt(3);
     best_before = offerDetailsList.elementAt(7);
     distance = "1.5 km"; // todo
+    this.city = "Deventer"; //todo
     image = offerDetailsList.elementAt(5);
     description = offerDetailsList.elementAt(4);
   }
 
+  //displays all the information of the offer
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -50,12 +57,15 @@ class _offerDetails extends State<offerDetails> {
           // to avoid overfloww
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment:
+                CrossAxisAlignment.start, // so the text starts at the left
             children: <Widget>[
               Image.network(
+                //first we load the image
                 image,
               ),
               Padding(
+                //some space between the image and the title
                 padding: const EdgeInsets.all(15.0),
                 child: Text(title,
                     textAlign: TextAlign.left,
@@ -68,7 +78,46 @@ class _offerDetails extends State<offerDetails> {
               Padding(
                 padding: const EdgeInsets.only(left: 15.0),
                 child: Row(
-                  children: const [Icon(FontAwesome.map_pin)],
+                  children: [
+                    const Icon(
+                      FontAwesome.map_pin,
+                      size: 15,
+                    ),
+                    Text("   " +
+                        city +
+                        "    "), // putting some distance between the icon and the location
+                    Text(
+                      "(" + distance + ")",
+                      style: const TextStyle(
+                          fontFamily: 'JosefinSans', color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0, top: 7),
+                child: Row(
+                  children: [
+                    const Icon(
+                      FontAwesome.calendar,
+                      size: 15,
+                    ),
+                    Text("   " +
+                        best_before +
+                        "    "), // putting some distance between the icon and the location
+                    const Text(
+                      "(best before)",
+                      style: TextStyle(
+                          fontFamily: 'JosefinSans', color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                // divider of the text (grey line)
+                padding: EdgeInsets.only(top: 8.0, left: 15, right: 15),
+                child: Divider(
+                  thickness: 1.5,
                 ),
               )
             ],
